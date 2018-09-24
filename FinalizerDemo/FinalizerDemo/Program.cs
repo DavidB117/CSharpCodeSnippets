da4e41a10a1e05d23bbed5207dea24acc895ec4a
david <dball@ivey.ca>
2018-09-24 13:39:36 -0400

    Added FinalizerDemo project.

---
+++ FinalizerDemo/FinalizerDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizerDemo
+{
+    class Program
+    {
+        class First
+        {
+            ~First()
+            {
+                System.Diagnostics.Trace.WriteLine("First's destructor is called.");
+            }
+        }
+
+        class Second : First
+        {
+            ~Second()
+            {
+                System.Diagnostics.Trace.WriteLine("Second's destructor is called.");
+            }
+        }
+
+        class Third : Second
+        {
+            ~Third()
+            {
+                System.Diagnostics.Trace.WriteLine("Third's destructor is called.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Destructor text is output to VS output window
+            Third t = new Third();
+        }
+    }
+}
